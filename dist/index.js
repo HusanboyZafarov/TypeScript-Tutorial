ab63add04467ee85a2e6c2f136d6f37e8da566f2
Husanboy Zafarov <115022906+HusanboyZafarov@users.noreply.github.com>
2024-10-23 16:30:02 +0500

    updating and finishing advanced types

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,4 @@
 "use strict";
-function calculateTax(income, taxYear = 2022) {
-    if (taxYear <= 2022)
-        return income * 1.2;
-    return income * 1.3;
-}
-console.log(calculateTax(10000, 2023));
+let value = "a";
+if (typeof value === "string")
+    console.log(value);
--- src/index.ts
+++ src/index.ts
@@ -149,3 +149,155 @@
 // Error, we cannot add key and value if it does not exist in object
 // employee.name = "Safir"
 // Property 'name' does not exist on type '{ id: number; }'.
+
+// Advanced topics
+
+// Union types
+
+// Little information: A Union Type in TypeScript allows you to specify
+// that a variable can hold one of multiple types. It's useful when you want to assign a
+// value that could be more than one type. Essentially, it provides flexibility by enabling
+// a variable to accept different types of data.
+
+// function kgToLbs(weight: number | string) {
+//   if (typeof weight === "number") return weight * 2.2;
+//   else return parseInt(weight) * 2.2;
+// }
+
+// console.log(kgToLbs("10"));
+
+// Intersection types
+
+// Little information: Intersection Types in TypeScript combine multiple types into one.
+// While union types allow a value to be one of several types, intersection types
+// require that a value satisfies all the combined types. Essentially,
+// an intersection type merges properties from different types into one.
+
+// let weight: number & string;
+
+// type Draggable = {
+//   drag: () => void;
+// };
+
+// type Resizable = {
+//   resize: () => void;
+// };
+
+// type UIWidget = Draggable & Resizable;
+
+// let texBox: UIWidget = {
+//   drag: () => {},
+//   resize: () => {},
+// };
+
+// Literal types
+// type Quantity = 50 | 100;
+// let quantity: Quantity = 100;
+
+// type Metric = "cm" | "inch";
+// let size: Metric = "cm";
+// console.log(`10${size}`);
+
+// Nullable types
+// function greet(name: string | null) {
+//     if(name)
+//         console.log(name.toUpperCase());
+//     else
+//         console.log("Hello!");
+// }
+
+// greet(null);
+
+// Option changing
+// type Customer = {
+//   birthday?: Date;
+// };
+
+// function getCustomer(id: number): Customer | null | undefined {
+//   return id === 0 ? null : { birthday: new Date() };
+// }
+// let customer = getCustomer(1);
+
+// // Optional property access operator
+// console.log(customer?.birthday?.getFullYear( ));
+
+// Type Assertation
+// let phone = <HTMLInputElement>document.getElementById("phone");
+// we use it when we know more about the type of calling element than type script
+
+// Unknown type
+// function render(document: unknown) {
+//   // Type Narrowing
+//   if (typeof document === "string") {
+//     document.toLowerCase();
+//   }
+//   document.fly();
+//   document.whateverWeWant();
+//   document.move();
+// }
+
+// Never type
+// function reject(message: string): never {
+//   throw new Error(message);
+// }
+
+// function processEvents(): never {
+//   while (true) {
+//     // Read a message from a queue
+//   }
+// }
+
+// processEvents();
+// reject("....");
+// console.log("Hello World!");
+
+// Exercises
+// type User = {
+//   name: string;
+//   age: number;
+//   occupation?: string;
+// };
+
+// let user: User[] = [
+//   {
+//     name: "John",
+//     age: 20,
+//     occupation: "Software developer",
+//   },
+//   {
+//     name: "Safir",
+//     age: 18,
+//   },
+// ];
+
+// Birds fly. Fish swim. A Pet can be a Bird or Fish. Use type aliases to represent these
+// type Bird = {
+//   fly: () => void;
+// };
+
+// type Fish = {
+//   swim: () => void;
+// };
+
+// type Pet = Bird | Fish;
+
+// Define a type for representing the days of week. Valid values are “Monday”, “Tuesday”,
+// etc.
+
+// type DayOfWeek =
+//   | "Monday"
+//   | "Tuesday"
+//   | "Wednesday"
+//   | "Thursday"
+//   | "Friday"
+//   | "Saturday"
+//   | "Sunday";
+
+// let user = getUser();
+// console.log(user?.adress?.street);
+// let x = foo !== null && foo !== undefined ? foo : bar();
+
+// let x = foo ?? bar();
+
+// let value: unknown = "a";
+// if (typeof value === "string") console.log(value);
