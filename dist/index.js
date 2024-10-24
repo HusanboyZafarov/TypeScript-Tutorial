717a0ecb2f022e0ca55389d82e317415d091f7a7
Husanboy Zafarov <zhzr1222@gmail.com>
2024-10-24 07:48:16 +0500

    updating and adding object-oriented programming

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,18 @@
 "use strict";
-let value = "a";
-if (typeof value === "string")
-    console.log(value);
+class Account {
+    constructor(id, owner, balance) {
+        this.id = id;
+        this.owner = owner;
+        this._balance = balance;
+    }
+    deposit(amount) {
+        if (amount <= 0)
+            throw new Error('Invalid amount');
+        this._balance += amount;
+    }
+    getBalance() {
+        return this._balance;
+    }
+}
+let account = new Account(1, 'Safir', 0);
+console.log(account.getBalance());
--- src/index.ts
+++ src/index.ts
@@ -167,7 +167,6 @@
 // console.log(kgToLbs("10"));
 
 // Intersection types
-
 // Little information: Intersection Types in TypeScript combine multiple types into one.
 // While union types allow a value to be one of several types, intersection types
 // require that a value satisfies all the combined types. Essentially,
@@ -191,6 +190,12 @@
 // };
 
 // Literal types
+// Little information: Literal types in TypeScript allow you to define
+// a variable that can only have a specific value.For example, you can set a variable
+// to accept only a few predefined values, like specific strings or numbers.
+// This restricts the variable to a certain set of values, increasing the
+// precision of your code.
+
 // type Quantity = 50 | 100;
 // let quantity: Quantity = 100;
 
@@ -199,6 +204,11 @@
 // console.log(`10${size}`);
 
 // Nullable types
+// Little information: Nullable types allow variables to be explicitly assigned
+// null or undefined along with their other types.In TypeScript,
+// you can use the union type null or undefined with another type
+// to define variables that can have those values.
+
 // function greet(name: string | null) {
 //     if(name)
 //         console.log(name.toUpperCase());
@@ -209,6 +219,15 @@
 // greet(null);
 
 // Option changing
+// Little information: "Option changing" isn't a specific
+// TypeScript term, but it may refer to enabling or disabling strict options
+// (like strictNullChecks) in TypeScript’s configuration.
+// This can affect how the compiler handles nullable types, implicit any,
+// or other checks.
+// Use cases: Adjusting TypeScript's options is appropriate when you want
+//  stricter or looser type - checking based on your project needs.
+//  strictNullChecks for example ensures you handle null and undefined correctly.
+
 // type Customer = {
 //   birthday?: Date;
 // };
@@ -222,10 +241,18 @@
 // console.log(customer?.birthday?.getFullYear( ));
 
 // Type Assertation
+// Little information: Type assertions allow you to tell TypeScript to treat a
+// variable as a specific type, overriding its inferred or declared type.
+// This is useful when you know more about a value’s type than TypeScript does.
+
 // let phone = <HTMLInputElement>document.getElementById("phone");
 // we use it when we know more about the type of calling element than type script
 
 // Unknown type
+// Little information: The unknown type is a safer alternative to any,
+// indicating that the type of a variable is unknown.Unlike any, values
+// of type unknown must be type - checked before you can perform any operations on them.
+
 // function render(document: unknown) {
 //   // Type Narrowing
 //   if (typeof document === "string") {
@@ -237,6 +264,10 @@
 // }
 
 // Never type
+// Little information: The never type represents values that never occur,
+// such as in functions that throw an error or never return.It is typically
+// used for functions that stop the program or in unreachable code.
+
 // function reject(message: string): never {
 //   throw new Error(message);
 // }
@@ -301,3 +332,87 @@
 
 // let value: unknown = "a";
 // if (typeof value === "string") console.log(value);
+
+// Object-oriented programming
+// Object-Oriented Programming (OOP) is a programming paradigm based on the
+// concept of "objects," which can contain both data and functions.
+// The goal of OOP is to structure programs around objects rather than actions,
+// and data rather than logic.In OOP, objects are instances of classes,
+// which are blueprints that define the structure and behavior of the objects.
+
+// Creating classes
+// class Account {
+//   id: number;
+//   owner: string;
+//   balance: number;
+
+//   constructor(id: number, owner: string, balance: number) {
+//     this.id = id;
+//     this.owner = owner;
+//     this.balance = balance;
+//   }
+
+//   deposit(amount: number): void {
+//     if (amount <= 0) throw new Error('Invalid amount');
+//     this.balance += amount;
+//   }
+// }
+
+// let account = new Account(1, 'Safir', 0)
+// account.deposit(1000)
+// console.log(account instanceof Account);
+
+// ReadOnly and Optional properties
+// class Account {
+//   readonly id: number;
+//   owner: string;
+//   balance: number;
+//   nickname?: string;
+
+//   constructor(id: number, owner: string, balance: number) {
+//     this.id = id;
+//     this.owner = owner;
+//     this.balance = balance;
+//   }
+
+//   deposit(amount: number): void {
+//     if (amount <= 0) throw new Error('Invalid amount');
+//     this.balance += amount;
+//
+//     ERROR
+//     this.id = 0
+//   }
+// }
+
+// Access Controlling keywords
+// In typeScript we have 3 modifiers
+// Public, Private, Protected
+
+// class Account {
+//   id: number;
+//   owner: string;
+//   private _balance: number;
+//   nickname?: number;
+
+//   constructor(id: number, owner: string, balance: number) {
+//     this.id = id;
+//     this.owner = owner;
+//     this._balance = balance;
+//   }
+
+//   deposit(amount: number): void {
+//     if (amount <= 0) throw new Error('Invalid amount');
+//     this._balance += amount;
+//   }
+
+//   private calculateTax() {}
+
+//   getBalance(): number {
+//     return this._balance;
+//   }
+// }
+
+// let account = new Account(1, 'Safir', 0);
+// console.log(account.getBalance());
+// Error
+// account.calculateTax()
